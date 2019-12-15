0b1d05d9327f8756038dd7fde03c173b69915b9a
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2019-12-15 09:49:48 -0600

    added PORT and listen to, scraping articles and trailers , send json to localhost:PORT

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const
   axios = require('axios'),
   cheerio = require('cheerio');
 
-const app = express();
+const 
+  app = express(),
+  PORT = process.env.PORT || 3500;
 
 const 
   dbURL = 'lastestInNews',
@@ -13,55 +15,115 @@ const
 const db = mongojs(dbURL, collections)
 db.on("error", error => console.log(`Database Error: ${error}`));
 
-
-console.log('*****Scarpping from Cinemblend *****');
-axios.get('https://www.cinemablend.com/news.php').then(response => {
-// axios.get('https://old.reddit.com/r/webdev/').then(response => {
-  // console.log(response.data)
-  let $ = cheerio.load(response.data)
-  
-  
-  const results = [];
-  $('div.story_item').each((i, element) => {
-
-    let 
-    headline = $(element)
-      .children('a')
-      .attr('title'),
-    summary = $(element)
-      .children('div.content')
-      .children('div.story_summary')
-      .children('p').text(),
-    url = $(element)
-      .children('a')
-      .attr('href'), 
-    author = $(element)
-      .children('div.author')
-      .children('.story_author').text(),
-      
-    date = $(element)
-      .children('.author')
-      .children('.story_published').text(),
-    img = $(element)
-      .children('a')
-      .children('img')['0']
-      .attribs.src;
-   
-    
-
-    results.push({
-      i,
-      headline,
-      summary,
-      url,
-      author,
-      date,
-      img
-     
-
-    });
-  });
-
-  // Log the results once you've looped through each of the elements found with cheerio
-  console.log(results[0]);
+app.get('/', (req, res) => {
+  res.send("HomePage")
 })
+
+app.get('/scrape-articles', (req, res) => {
+  console.log('*****Scarpping Articles from Cinemblend *****');
+
+  const results = []; 
+
+  axios.get('https://www.cinemablend.com/news.php').then(response => {
+
+    let $ = cheerio.load(response.data)
+
+    $('div.story_item').each((i, element) => {
+
+      let 
+        headline = $(element)
+          .children('a')
+          .attr('title'),
+        summary = $(element)
+          .children('div.content')
+          .children('div.story_summary')
+          .children('p').text(),
+        url = $(element)
+          .children('a')
+          .attr('href'), 
+        author = $(element)
+          .children('div.author')
+          .children('.story_author').text(),    
+        date = $(element)
+          .children('.author')
+          .children('.story_published').text(),
+        img = $(element)
+          .children('a')
+          .children('img')['0']
+          .attribs.src;
+   
+      results.push({
+        i,
+        headline,
+        summary,
+        url,
+        author,
+        date,
+        img
+      
+      });
+    });
+
+    // Log the results once you've looped through each of the elements found with cheerio
+    console.log(results[0]);
+    res.json(results)
+  })
+  
+})
+
+app.get('/scrape-trailers', (req, res) => {
+  console.log('*****Scarpping Trailers from Cinemblend *****');
+
+  const results = []; 
+
+  axios.get('https://www.cinemablend.com/trailers/').then(response => {
+
+    let $ = cheerio.load(response.data)
+    $('div.story_item').each((i, element) => {
+      
+      let 
+        headline = $(element)
+          .children('a')
+          .attr('title'),
+        summary = $(element)
+          .children('div.content')
+          .children('div.story_summary')
+          .children('p').text(),
+        url = $(element)
+          .children('a')
+          .attr('href'), 
+        author = $(element)
+          .children('div.author')
+          .children('.story_author').text(),    
+        date = $(element)
+          .children('.author')
+          .children('.story_published').text(),
+        img = $(element)
+          .children('a')
+          .children('img')['0']
+          .attribs.src;
+   
+      results.push({
+        i,
+        headline,
+        summary,
+        url,
+        author,
+        date,
+        img
+      
+      });
+    });
+
+    // Log the results once you've looped through each of the elements found with cheerio
+    console.log(results[0]);
+
+    
+    res.json(results)
+  })
+
+
+})
+
+
+app.listen(PORT, () => {console.log(`App running on http://localhost:${PORT}`)})
